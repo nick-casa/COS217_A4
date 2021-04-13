9c84710a18c9f2ea58100195552ac561fc345416
nick-casa <nc16@princeton.edu>
2021-04-13 17:09:57 -0400

    `Added to checkerDT.c`

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -37,6 +37,7 @@ static Node_T FT_traversePathFrom(char* path, Node_T curr, nodeType type) {
         for(i = 0; i < Node_getNumDirChildren(curr); i++) {
             found = FT_traversePathFrom(path,
                                         Node_getChildDirectory(curr, i), type);
+
             if(found != NULL) return found;
         }
         for(i = 0; i < Node_getNumFileChildren(curr); i++) {

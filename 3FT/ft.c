6e1d7484925a98bb6597bdd03282d9effe366da7
nick-casa <nc16@princeton.edu>
2021-04-13 17:18:37 -0400

    Changes by alinachen

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -366,13 +366,13 @@ static size_t FT_preOrderTraversal(Node_T n, DynArray_T d, size_t i) {
     assert(d != NULL);
 
     if(n != NULL) {
+        (void) DynArray_set(d, i, Node_getPath(n));
+        i++;
         for(a = 0; a < Node_getNumFileChildren(n); a++){
             file = Node_getChildFile(n, a);
             (void) DynArray_set(d, i, Node_getPath(file));
             i++;
         }
-        (void) DynArray_set(d, i, Node_getPath(n));
-        i++;
         for(c = 0; c < Node_getNumDirChildren(n); c++) {
             i = FT_preOrderTraversal(Node_getChildDirectory(n, c), d, i);xx
         }

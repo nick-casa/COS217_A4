87b33754fe940909bb288984e812476d9c5e544b
nick-casa <nc16@princeton.edu>
2021-04-13 19:36:08 -0400

    `Added to checkerDT.c`

--- 3FT/node.c
+++ 3FT/node.c
@@ -325,8 +325,7 @@ int Node_linkChild(Node_T parent, Node_T child) {
    child->parent = parent;
    if(child->type == ISDIRECTORY){
        if(DynArray_bsearch(parent->dirChildren, child, &i,
-             (int (*)(const void*, const void*))
-             ) == 1) {
+             (int (*)(const void*, const void*))) == 1) {
           assertNodes(parent,child);
           return ALREADY_IN_TREE;
        }
